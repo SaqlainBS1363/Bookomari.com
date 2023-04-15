2e77682e935ed60176c7855a46871b50ef707dfd
Md. Anwarul Habib <anwarul.habib@brainstation-23.com>
2023-04-15 12:13:16 +0600

    Adds Author and Book class from development_F1

---
+++ Models/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookomari.com.Models
+{
+    public class Author
+    {
+        public string AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string Address { get; set; }
+        public byte[] AuthorPhoto { get; set; }
+        public List<Book> Books { get; set; }
+
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Bookomari.com.Models
+{
+    public class Book
+    {
+        public string BookId { get; set; }
+        public string Language { get; set; }
+        public byte[] BookCoverPhoto { get; set; }
+    }
+}
